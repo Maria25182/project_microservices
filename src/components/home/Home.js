d3307ddd041ff627d4453c400b265c9cb5585c41
Maria Cardona <maria212502@gmail.com>
2021-03-25 18:14:30 -0500

    Primera_comit

---
+++ src/components/home/Home.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+    return (
+
+        <div class="container">
+            <p class="lead">Just my entry in yet another Codepen challenge.</p>
+            <div class="header-bar">
+                <h1 class="logo">C</h1>
+                <ul class="slider-menu">
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Services</li>
+                </ul>
+            </div>
+
+
+
+        </div>
+    )
+}
+
+export default Home
