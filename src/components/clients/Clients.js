6b4246a790feba47b7cab74edb76f9dab4f76b2f
Maria Cardona <maria212502@gmail.com>
2021-03-25 22:46:40 -0500

    mod25/03

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -8,15 +8,31 @@ const Clients = () => {
 
         <div class="Principal">
             <h1>Lista de usuarios</h1>
+            <div class= "content_2">
+                <p>Lorem ipsum d</p>
+            </div>
             <form class="Form">
-                <label for="nombre">Nombre</label>
-                <input type="text" name="nombre" id="nombre" placeholder="Nombres" />
-                <label for="apellidos">Apellidos</label>
-                <input type="text" name="apellidos" id="apellidos" placeholder="Apellidos" />
-                <label for="apellidos">Email</label>
-                <input type="email" name="email" id="email" placeholder="email" required />
+                <div>
+                    <label for="nombre">Nombre</label>
+                    <br />
+                    <input type="text" name="nombre" id="nombre" placeholder="Nombres" />
+                </div>
+                <div>
+                    <label for="apellidos">Apellidos</label>
+                    <br />
+                    <input type="text" name="apellidos" id="apellidos" placeholder="Apellidos" />
+                </div>
+                <div>
+                    <label for="apellidos">Email</label>
+                    <br />
+                    <input type="email" name="email" id="email" placeholder="email" required />
+                </div>
+                <br />
                 <input type="submit" name="enviar" value="Guardar" />
+
+             
             </form>
+           
         </div >
     )
 }
