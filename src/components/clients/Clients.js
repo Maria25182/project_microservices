52e8b7b1f207b16fa7a044657474bacde98ec627
Maria Cardona <maria212502@gmail.com>
2021-03-25 20:52:54 -0500

    PrimeraModFondo_Prueba_Maria

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -5,7 +5,8 @@ import   './Clients.css';
 const Clients = () => {
     return (
         <div class="Principal">
-            <h1 id="h1">Hola</h1>
+            <h1 id="h1">Form</h1>
+
         </div>
     )
 }
