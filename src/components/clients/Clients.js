2e9a3ef8698c2374f10a1aa01d2fbfe085ae635a
Maria Cardona <maria212502@gmail.com>
2021-03-25 21:37:36 -0500

    form_1_maria

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -4,15 +4,20 @@ import './Clients.css';
 
 const Clients = () => {
     return (
+
+
         <div class="Principal">
-            <form class= "Form">
-                <label>
-                    Name:
-    <input type="text" name="name" />
-                </label>
-                <input type="submit" value="Submit" />
+            <h1>Lista de usuarios</h1>
+            <form class="Form">
+                <label for="nombre">Nombre</label>
+                <input type="text" name="nombre" id="nombre" placeholder="Nombres" />
+                <label for="apellidos">Apellidos</label>
+                <input type="text" name="apellidos" id="apellidos" placeholder="Apellidos" />
+                <label for="apellidos">Email</label>
+                <input type="email" name="email" id="email" placeholder="email" required />
+                <input type="submit" name="enviar" value="Guardar" />
             </form>
-        </div>
+        </div >
     )
 }
 
