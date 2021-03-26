0821e1d26aee5c09be924e1d6adc9b121d4386db
Maria Cardona <maria212502@gmail.com>
2021-03-25 20:31:55 -0500

    menu_organizado_maria

---
+++ src/Routes.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Clients from './components/clients/Clients.js'
+import Sales from './components/sales/Sales.js'
+
+import Invoice from './components/invoice/Invoice.js'
+import Products from './components/products/Products.js'
+import Home from './components/home/Home.js'
+
+
+const Routes = () => {
+    return (
+        <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/Clients' component={Clients} />
+            <Route exact path='/Products' component={Products} />
+            <Route exact path='/Sales' component={Sales} />
+        </Switch>
+
+    );
+}
+
+export default Routes;
