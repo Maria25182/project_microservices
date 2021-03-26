ec1ab3680a8048f6ec8dd5c0de2ebe0272b5b433
Andres Gallo <afgg200115@gmail.com>
2021-03-25 23:34:14 -0500

    Agg primer diseño formulario Andres

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -1,10 +1,23 @@
 import React from 'react'
-
+import './Products.css';
 const Products = () => {
     return (
-        <div>
+        <div className="form-container">
             
+        <div className="form-box">
+            <div className="form-product">
+                <form className="form-product-btn">
+                    < label htmlFor="productname">Product name</label><br/>
+                    <input name="productname" id="productname" type="text" placeholder="Write the name" /><br/>
+                
+                    <label htmlFor="productprice">Price</label><br/>
+                    <input name="productprice" id="productprice" type="text" placeholder="Write the price" /><br/>
+
+                    <input className='btn_enviar' name="productsubmit" id="productsubmit" type="submit" value="Enviar" /><br/>
+                </form>
+            </div>
         </div>
+    </div>
     )
 }
 
